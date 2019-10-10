6b23e3abee6a032d879d1f37822a42c37a9356a7
Tomas Zulberti <tomas@jampp.com>
2019-10-10 17:37:36 -0300

    CORE-2699: Improved function to parse requirements file

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 import os
 from setuptools import setup, find_packages
 
-requirements = open('requirements.txt').read(-1).split()
-
 setup_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(setup_dir, 'db_utils', 'VERSION'), 'r') as vf:
     version = vf.read().strip()
+
+
+def parse_requirements_txt(filename='requirements.txt'):
+    requirements = open(os.path.join(os.path.dirname(__file__), filename)).readlines()
+    # remove whitespaces
+    requirements = [line.strip().replace(' ', '') for line in requirements]
+    # remove all the requirements that are comments
+    requirements = [line for line in requirements if not line.startswith('#')]
+    # remove empty lines
+    requirements = list(filter(None, requirements))
+    return requirements
 
 
 setup(
@@ -15,7 +24,7 @@ setup(
     version=version,
     description='Run the database migrations',
     author='Jampp',
-    install_requires=requirements,
+    install_requires=parse_requirements_txt(),
     entry_points={
         'console_scripts': {
             'db-utils = db_utils.main:main'
