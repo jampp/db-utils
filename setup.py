81f886898a0cf5b3a4098dccc5b108197c8bd983
Tomas Zulberti <tomas@jampp.com>
2019-10-11 13:27:36 -0300

    CORE-2699: Improve parse requirements function

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@
 import os
 from setuptools import setup, find_packages
 
-setup_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(setup_dir, 'db_utils', 'VERSION'), 'r') as vf:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(current_dir, 'db_utils', 'VERSION'), 'r') as vf:
     version = vf.read().strip()
 
 
 def parse_requirements_txt(filename='requirements.txt'):
-    requirements = open(os.path.join(os.path.dirname(__file__), filename)).readlines()
-    # remove whitespaces
-    requirements = [line.strip().replace(' ', '') for line in requirements]
-    # remove all the requirements that are comments
-    requirements = [line for line in requirements if not line.startswith('#')]
-    # remove empty lines
-    requirements = list(filter(None, requirements))
-    return requirements
+    with open(os.path.join(current_dir, filename)) as requirements_file:
+        requirements = requirements_file.readlines()
+        # remove whitespaces
+        requirements = [line.strip().replace(' ', '') for line in requirements]
+        # remove all the requirements that are comments
+        requirements = [line for line in requirements if not line.startswith('#')]
+        # remove empty lines
+        requirements = list(filter(None, requirements))
+        return requirements
 
 
 setup(
