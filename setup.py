d44e23914370fdfefcc095a23e1e01d3b4bf0d43
Tomas Zulberti <tomas@jampp.com>
2019-10-28 17:46:07 -0300

    CORE-2700: Add requirements-dev to the package data

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
         'Topic :: Software Development',
     ],
     license='BSD 3-Clause',
-    package_data={'': ['VERSION', 'requirements.txt', 'LICENSE']},
+    package_data={'': ['VERSION', 'requirements.txt', 'LICENSE', 'requirements-dev.txt']},
     test_suite='tests',
 )
